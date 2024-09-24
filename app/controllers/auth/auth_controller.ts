b35d816e9acd26e97a6bc484b795ed45b26b7203
IanBraga96 <contatoianbraga@gmail.com>
2024-09-24 18:46:48 -0300

    Adicionado rota signIn

--- app/controllers/auth/auth_controller.ts
+++ app/controllers/auth/auth_controller.ts
@@ -2,14 +2,25 @@ import { inject } from '@adonisjs/core'
 import type { HttpContext } from '@adonisjs/core/http'
 import { signUpValidator } from '#validators/auth/sign_up_and_sign_in_user'
 import SignUpUseCase from '../../use_case/auth/sign_up_use_case.js'
+import SignInUseCase from '../../use_case/auth/sign_in_use_case.js'
+import { signInValidator } from '#validators/auth/sign_up_and_sign_in_user'
 
 @inject()
 export default class AuthController {
-  constructor(protected signUpUseCase: SignUpUseCase) {}
+  constructor(
+    protected signUpUseCase: SignUpUseCase,
+    protected signInUseCase: SignInUseCase
+  ) {}
 
   async signUp({ request, response }: HttpContext) {
     const userDto = await request.validateUsing(signUpValidator)
     const data = await this.signUpUseCase.run(userDto)
     return response.created(data)
+  }
+
+  async signIn({ request, response }: HttpContext) {
+    const signInDto = await request.validateUsing(signInValidator)
+    const data = await this.signInUseCase.run(signInDto)
+    return response.ok(data)
   }
 }
--- start/routes/auth/auth.ts
+++ start/routes/auth/auth.ts
@@ -5,5 +5,6 @@ const AuthController = () => import('#controllers/auth/auth_controller')
 router
   .group(() => {
     router.post('auth/signUp', [AuthController, 'signUp'])
+    router.post('auth/signIn', [AuthController, 'signIn'])
   })
   .prefix('/development')
